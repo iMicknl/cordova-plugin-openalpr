4450a951ccfd871d2c78158d1763e636bd47e346
Mick Vleeshouwer <mick@codeconverters.nl>
2016-10-12 16:04:11 +0200

    Update code

--- src/ios/OpenALPR.mm
+++ src/ios/OpenALPR.mm
@@ -6,16 +6,42 @@
 @implementation OpenALPR
 
 -(void) scan:(CDVInvokedUrlCommand *)command {
-    NSArray *dataArray = @[@"AA12BB", @"Test"];
+    //NSArray *dataArray = @[@"AA12BB", @"Test"];
 
-    //move to constructor?
     self.plateScanner = [[PlateScanner alloc] init];
-    //self.plates = [NSMutableArray arrayWithCapacity:0];
+    //self.plateScanner = [PlateScanner new];
+    self.plates = [NSMutableArray arrayWithCapacity:0];
 
+    //Temporary use hardcoded filepath
+    NSString *plateFilename = @"nl-optie2.jpg";
+    NSString *imagePath = [[NSBundle mainBundle] pathForResource:plateFilename ofType:nil];
+
+    //Check if imagePath exists
+    if (imagePath) {
+        cv::Mat image = imread([imagePath UTF8String], CV_LOAD_IMAGE_COLOR);
+
+        [self.plateScanner
+         scanImage:image
+         onSuccess:^(NSArray * results) {
+             [self.plates addObjectsFromArray:results];
+
+             NSLog(@"Success!" );
+             NSLog(@"Number of items in my array is: %lu", (unsigned long)[results count]);
+         }
+         onFailure:^(NSError * error) {
+             dispatch_async(dispatch_get_main_queue(), ^{
+                 NSLog(@"Error: %@", [error localizedDescription]);
+            });
+         }];
+
+    }
+    else {
+        NSLog(@"Error :(");
+    }
 
     CDVPluginResult *pluginResult = [ CDVPluginResult
                                       resultWithStatus    : CDVCommandStatus_OK
-                                      messageAsArray : dataArray];
+                                      messageAsArray : self.plates];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
