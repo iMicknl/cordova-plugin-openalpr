c765b08fbc740468c77d98a3c274b4b0834949d2
Mick Vleeshouwer <mick@codeconverters.nl>
2016-10-12 15:39:52 +0200

    Update iOS compatibiltiy

--- src/ios/OpenALPR.mm
+++ src/ios/OpenALPR.mm
@@ -1,8 +1,18 @@
 #import "OpenALPR.h"
 
+#import "PlateScanner.h"
+#import "Plate.h"
+
 @implementation OpenALPR
+
 -(void) scan:(CDVInvokedUrlCommand *)command {
     NSArray *dataArray = @[@"AA12BB", @"Test"];
+
+    //move to constructor?
+    self.plateScanner = [[PlateScanner alloc] init];
+    //self.plates = [NSMutableArray arrayWithCapacity:0];
+
+
     CDVPluginResult *pluginResult = [ CDVPluginResult
                                       resultWithStatus    : CDVCommandStatus_OK
                                       messageAsArray : dataArray];
