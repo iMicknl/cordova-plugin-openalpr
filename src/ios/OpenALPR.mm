13cb42e0f1338ef92e05e98f28e6afc0515300d7
Melvin Versluijs <melvin.versluijs@hotmail.com>
2016-10-17 18:55:16 +0200

    Moved the scan to a different thread. (#4)

--- src/ios/OpenALPR.mm
+++ src/ios/OpenALPR.mm
@@ -7,32 +7,33 @@
 
 -(void) scan:(CDVInvokedUrlCommand *)command {
 
-    //TODO Move to subthread
-    self.plateScanner = [[PlateScanner alloc] init];
-    self.plates = [NSMutableArray arrayWithCapacity:0];
+    [self.commandDelegate runInBackground:^{
+        self.plateScanner = [[PlateScanner alloc] init];
+        self.plates = [NSMutableArray arrayWithCapacity:0];
 
-    NSString* imagePath = [command.arguments objectAtIndex:0];
-    CDVPluginResult* pluginResult = nil;
+        NSString* imagePath = [command.arguments objectAtIndex:0];
+        CDVPluginResult* pluginResult = nil;
 
-    //TODO Check if necessary
-    imagePath = [imagePath stringByReplacingOccurrencesOfString:@"file://" withString:@""];
+        //TODO Check if necessary
+        imagePath = [imagePath stringByReplacingOccurrencesOfString:@"file://" withString:@""];
 
-    //Check if imagePath exists
-    if (imagePath) {
-        cv::Mat image = imread([imagePath UTF8String], CV_LOAD_IMAGE_COLOR);
+        //Check if imagePath exists
+        if (imagePath) {
+            cv::Mat image = imread([imagePath UTF8String], CV_LOAD_IMAGE_COLOR);
 
-        [self.plateScanner
-         scanImage:image
-         onSuccess:^(NSArray * results) {
+            [self.plateScanner
+             scanImage:image
+             onSuccess:^(NSArray * results) {
 
-             for(Plate* plate in results) {
-                 NSDictionary *dic = @{
-                                       @"number" : plate.number,
-                                       @"confidence" : [NSNumber numberWithFloat:plate.confidence]
-                                       };
-                 [self.plates addObject:dic];
+                 for(Plate* plate in results) {
+                     NSDictionary *dic = @{
+                                           @"number" : plate.number,
+                                           @"confidence" : [NSNumber numberWithFloat:plate.confidence]
+                                           };
+                     [self.plates addObject:dic];
+                 }
              }
-         }
+
          onFailure:^(NSError * error) {
              dispatch_async(dispatch_get_main_queue(), ^{
                  NSLog(@"Error: %@", [error localizedDescription]);
@@ -42,13 +43,14 @@
         pluginResult = [ CDVPluginResult
                                          resultWithStatus    : CDVCommandStatus_OK
                                          messageAsArray: self.plates];
-    }
-    else {
-        NSLog(@"Error :(");
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Error in reading filepath"];
-    }
+        }
+        else {
+            NSLog(@"Error :(");
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Error in reading filepath"];
+        }
 
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
 }
 
 @end
